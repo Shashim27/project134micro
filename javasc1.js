c8616de12c0fa4e054b09aad9958d5c23d2d13c0
MD ABUL HASHIM <65435274+Shashim27@users.noreply.github.com>
2022-02-13 12:37:52 -0500

    Add files via upload

---
+++ javasc1.js
@@ -0,0 +1,61 @@
+/*window.onscroll = function() {myFunction()};
+
+function myFunction() {
+  var winScroll = document.body.scrollTop || document.documentElement.scrollTop;
+  var height = document.documentElement.scrollHeight - document.documentElement.clientHeight;
+  var scrolled = (winScroll / height) * 100;
+  document.getElementById("myBar").style.width = scrolled + "%";
+}*/
+
+
+var btn1=document.querySelector('#SUBMIT_BT');
+
+
+btn1.addEventListener('click',function(){
+  alert(' You are going to check out page!');
+
+});
+
+
+
+
+var btn2=document.querySelector('#SUBMIT_BT1');
+
+
+btn2.addEventListener('click',function(){
+  alert(' You are going to check out page!');
+
+
+});
+
+var btn3=document.querySelector('#SUBMIT_BT2');
+
+
+btn3.addEventListener('click',function(){
+  alert(' You are going to check out page!');
+
+});
+
+var btn4=document.querySelector('#SUBMIT_BT3');
+
+
+btn4.addEventListener('click',function(){
+  alert(' You are going to check out page!');
+
+});
+
+var btn5=document.querySelector('#SUBMIT_BT4');
+
+
+btn5.addEventListener('click',function(){
+  alert(' You are going to check out page!');
+
+});
+
+var btn6=document.querySelector('#SUBMIT_BT5');
+
+
+btn6.addEventListener('click',function(){
+  alert(' You are going to check out page!');
+
+});
